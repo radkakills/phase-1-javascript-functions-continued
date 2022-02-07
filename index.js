446b9b086ee207851f6207222879b1ace30b34f3
Jeffery Radka <jeffradka9@gmail.com>
2022-02-07 12:23:19 -0500

    Completed assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// code your solution here
+function saturdayFun(someActivity="roller-skate") {
+    return `This Saturday, I want to ${someActivity}!`
+    }
+    saturdayFun()
+    
+    let mondayWork = function(willDo="go to the office"){
+     return `This Monday, I will ${willDo}.` 
+    }
+    mondayWork()
+    
+    function wrapAdjective (style="*") {
+      return function(stringOne="special"){
+        return `You are ${style}${stringOne}${style}!`
+      }
+    }
+    
+    let Calculator = {
+      add: function (a,b){
+        return a+b
+      } ,
+      subtract: (a,b) => {
+        return a - b
+      } ,
+      multiply: (a,b) => {
+        return a * b
+      } ,
+      divide: (a,b) => {
+        return a / b
+      } ,
+    }
+    
+    
+    function actionApplyer (start, array){
+      let a = start
+        for(let i = 0; i < array.length; i++){
+           a = array[i](a)
+          }
+        return a
+    }
